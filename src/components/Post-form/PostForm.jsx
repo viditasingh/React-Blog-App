9536788db9926e1f105f7085dd9e6353043f7257
Vidita Singh <vidita1812@gmail.com>
2025-06-26 23:54:21 +0530

    create PostForm

---
+++ src/components/Post-form/PostForm.jsx
@@ -0,0 +1,9 @@
+import React, { useCallback } from 'react'
+import { useForm } from 'react-hook-form'
+import {Button, Input, Select, RTE} from '../index'
+
+export default function PostForm() {
+  return (
+    <div>PostForm</div>
+  )
+}
