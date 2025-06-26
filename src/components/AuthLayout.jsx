dafc199b9c1afab2d9324ccb61fe85e6a330231a
Vidita Singh <vidita1812@gmail.com>
2025-06-26 23:54:10 +0530

    create authLayout

---
+++ src/components/AuthLayout.jsx
@@ -0,0 +1,25 @@
+import React, { Children, useEffect, useState } from 'react'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router'
+import { LoadingPage } from './index'
+
+export default function AuthLayout({children, authentication = true}) {
+    //determine children show krne hai ya nahi
+    const [loader, setLoader] = useState(true)
+    const navigate = useNavigate()
+    const authStatus = useSelector(state => state.auth.satus)
+
+    useEffect(()=>{
+        
+        if (authentication && authStatus !== authentication) {
+            navigate("/login")
+        } else if(!authentication && authStatus !== authentication){
+            navigate ("/")
+        }
+
+        setLoader(false)
+
+    },[authStatus, navigate,authentication])
+
+  return loader ? <LoadingPage/> : <>{children}</>
+}
