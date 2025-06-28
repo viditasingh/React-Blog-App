d058e8eec85328746ae7d88cffa30222f25bfbc5
Vidita Singh <vidita1812@gmail.com>
2025-06-28 16:01:32 +0530

    add functionality to logout button

--- src/components/Header/LogoutButton.jsx
+++ src/components/Header/LogoutButton.jsx
@@ -2,13 +2,16 @@ import React from 'react'
 import { useDispatch } from 'react-redux'
 import authService from '../../appwrite/auth'
 import { logout } from '../../store/authSlice'
+import { useNavigate } from 'react-router';
 
 
 export default function LogoutButton() {
     const dispatch = useDispatch();
+    const navigate = useNavigate()
     const logoutHandler = () =>{
         authService.logout().then(()=>{
             dispatch(logout())
+            navigate('/')
         })
     }
   return (
