b53ded65323cc44ad53a33437d44e574dd91b47d
Vidita Singh <vidita1812@gmail.com>
2025-06-28 17:13:12 +0530

    add extensions

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import path from 'path'
 
-
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [tailwindcss(),react()],
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
+    },
+    extensions: ['.js', '.jsx', '.ts', '.tsx']
+  },
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
 })
