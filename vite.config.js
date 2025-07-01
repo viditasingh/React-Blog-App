528ca878c2eb47c50698648481f1e3b353e48d9b
Vidita Singh <vidita1812@gmail.com>
2025-07-01 08:34:41 +0530

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  build: { chunkSizeWarningLimit: 1600, },
   plugins: [react(), tailwindcss()],
 })
