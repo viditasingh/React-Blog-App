a4a06875d53b4311112bb006c4c154fd6a83c62a
Vidita Singh <vidita1812@gmail.com>
2025-07-01 08:32:11 +0530

    debug

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
+import tailwindcss from '@tailwindcss/vite'
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  },
-  build: {
-    rollupOptions: {
-      external: [],
-    },
-  },
+  plugins: [react(), tailwindcss()],
 })
